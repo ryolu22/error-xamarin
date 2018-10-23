c882f905276f75f353aad413c1155afd2df2ce3e
ryolu22 <44153960+ryolu22@users.noreply.github.com>
2018-10-23 13:03:46 +0700

    Ini mas
    Ini mas

---
+++ AplikasiModul2Kel412/app/src/main/java/com/example/rio/aplikasimodul2kel41/opokoekrungu.java
@@ -0,0 +1,13 @@
+package com.example.rio.aplikasimodul2kel41;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class opokoekrungu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opokoekrungu);
+    }
+}
